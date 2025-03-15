22c4723164bb54122be530438ffd1398740d193a
Tanzim vai <khandakertanzimmahtab@gmail.com>
2025-03-15 04:44:27 -0400

    chore: add Vercel configuration for frontend deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,18 +8,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,
-    minify: 'terser',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          mui: ['@mui/material', '@mui/icons-material']
-        },
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]'
-      }
-    }
+    minify: 'terser'
   },
   resolve: {
     alias: {
